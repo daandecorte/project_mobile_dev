afae02561c58714ce3a16915eb875a26764fc5ab
Brian Mattheyses <matth@LaPToP_BRiaN.(none)>
2025-12-08 03:28:09 +0100

    Full working chat without room db

--- app/src/main/java/edu/ap/project_mobile_dev/MainActivity.kt
+++ app/src/main/java/edu/ap/project_mobile_dev/MainActivity.kt
@@ -4,30 +4,24 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import com.google.firebase.FirebaseApp
 import edu.ap.project_mobile_dev.ui.login.LoginScreen
-import edu.ap.project_mobile_dev.ui.theme.Project_mobile_devTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -36,6 +30,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import edu.ap.project_mobile_dev.ui.add.AddScreen
 import edu.ap.project_mobile_dev.ui.activity.ActivityScreen
 import edu.ap.project_mobile_dev.ui.chat.ChatScreen
+import edu.ap.project_mobile_dev.ui.chats.ChatsScreen
 import edu.ap.project_mobile_dev.ui.home.HomeScreen
 import edu.ap.project_mobile_dev.ui.home.HomeViewModel
 import edu.ap.project_mobile_dev.ui.profile.ProfileScreen
@@ -73,19 +68,18 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable("home"){
-
-                         HomeScreen(
-                             onActivityClick = { activity -> navController.navigate("activity/${activity.documentId}") },
-                             onChatClick = { navController.navigate("chat") },
-                             onProfileClick = { navController.navigate("profile") },
-                             navController,
-                             homeViewModel
-                        )}
+                            HomeScreen(
+                                onActivityClick = { activity -> navController.navigate("activity/${activity.documentId}") },
+                                onChatClick = { navController.navigate("chats") },
+                                onProfileClick = { navController.navigate("profile") },
+                                navController,
+                                homeViewModel
+                            )
+                        }
                         composable("profile") {
                             ProfileScreen(
                                 navController,
-                                onBack = { navController.popBackStack()
-                                }
+                                onBack = { navController.popBackStack() }
                             )
                         }
                         composable("add") {
@@ -109,9 +103,19 @@ class MainActivity : ComponentActivity() {
                                 onBack = { navController.popBackStack() }
                             )
                         }
-                        composable("chat") {
+                        composable("chats") {
+                            ChatsScreen(
+                                onBack = { navController.popBackStack() },
+                                onChatClick = { chat -> navController.navigate("chat/${chat.id}") }
+                            )
+                        }
+                        composable("chat/{id}"){
+                            backStackEntry ->
+                            val id = backStackEntry.arguments?.getString("id") ?: "0"
+
                             ChatScreen(
-                                onBack = { navController.popBackStack() }
+                                id,
+                                onBack = { navController.popBackStack() },
                             )
                         }
                     }
---
+++ app/src/main/java/edu/ap/project_mobile_dev/ui/model/User.kt
@@ -0,0 +1,9 @@
+package edu.ap.project_mobile_dev.ui.model
+
+import android.graphics.Bitmap
+
+data class User (
+    val userId: String,
+    val username: String,
+    val bitmap: Bitmap?
+)
